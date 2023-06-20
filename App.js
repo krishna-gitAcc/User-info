2cefa82a0e98b59c2c8a6d9078c5e5f41fb54766
Krishna <krishna0512.kr@gmail.com>
2023-06-20 12:15:59 +0530

    my initial commit

--- App.js
+++ App.js
@@ -1,11 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import LoggedInPage from "./components/LoggedInPage";
+import LogInPage from "./components/LogInPage";
+import TryAgain from "./components/TryAgain";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:
+      "589998042377-sl3gctj5rajjgftnvahofs9dr2ri9ar7.apps.googleusercontent.com",
+    expoClientId:
+      "589998042377-f6nm48qae6bul95nlcg5gesp9r444sbv.apps.googleusercontent.com",
+    webClientId:
+      "589998042377-q5mapavue1sduuhipjdah28a80dcssb2.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      setToken(response.authentication.accessToken);
+      getUserInfo();
+    }
+  }, [response, token]);
+
+  const getUserInfo = async () => {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      console.log(user);
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {userInfo === null ? (
+        <LogInPage signIn={promptAsync} />
+      ) : userInfo.status === "Unauthenticated" ? (
+        <TryAgain signIn={promptAsync} />
+      ) : (
+        <LoggedInPage
+          name={userInfo.name}
+          email={userInfo.email}
+          photoUrl={userInfo.picture}
+          setUserInfo={setUserInfo}
+        />
+      )}
     </View>
   );
 }
@@ -13,8 +66,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
